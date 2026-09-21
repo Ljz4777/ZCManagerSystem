9c64cdebe367d958abc8cc1f35247880a4dbe156
Ljz4777 <425446420@qq.com>
2026-09-21 16:37:53 +0800

    东软实训9.21

--- src/com/neusoft/db/DBManager.java
+++ src/com/neusoft/db/DBManager.java
@@ -47,7 +47,7 @@ public class DBManager {
 			Class.forName(prop.getProperty("DRIVER"));
 			conn = DriverManager.getConnection(prop.getProperty("DBURL"),prop.getProperty("USERNAME"),prop.getProperty("USERPWD"));
 			//conn.setAutoCommit(false);//先这样简单自动提交，以后再业务层管理事务，就需要设置为false了
-			conn.setAutoCommit(true);
+			conn.setAutoCommit(false);
 			if (logger.isInfoEnabled()) {
 				logger.info("读取数据库配置文件成功！");
 			}
--- src/com/neusoft/view/TestJdbcTemplate.java
+++ src/com/neusoft/view/TestJdbcTemplate.java
@@ -4,9 +4,10 @@ import com.neusoft.db.DBManager;
 import com.neusoft.db.JdbcTemplate;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class TestJdbcTemplate {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         //1.连接数据库
         DBManager dbManager = DBManager.getInstance();
         Connection connection = dbManager.getConnection();//连接数据库
@@ -15,7 +16,10 @@ public class TestJdbcTemplate {
         //3.获取数据库的连接
         jt.setConnection(connection);//将DBManager中获取到的连接赋值给JdbcTemplate中的连接
         int isRight = jt.update("insert into guestbook(name,phone,email,title,content,createtime) values(?,?,?,?,?,?)",
-                "soloking","123456","emailsoloking@163.com","title","content","2026-09-21");
+                "solo","123456","emailsoloking@163.com","title","content","2026-09-21");
+        //手动提交事务
+        connection.commit();
+        //connection.rollback();
         System.out.println(isRight);
     }
 }
